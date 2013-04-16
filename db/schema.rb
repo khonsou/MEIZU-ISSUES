18ce192bc95c94047e56c797a9660d9b451bc245
Yi Zhang <yzhang.wa@gmail.com>
2013-04-16 13:08:28 +0800

    To save the result of "rails generate delayed_job:active_record"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121225220025) do
+ActiveRecord::Schema.define(:version => 20130416050730) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "container_id"
@@ -165,6 +165,22 @@ ActiveRecord::Schema.define(:version => 20121225220025) do
 
   add_index "custom_values", ["custom_field_id"], :name => "index_custom_values_on_custom_field_id"
   add_index "custom_values", ["customized_type", "customized_id"], :name => "custom_values_customized"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "documents", :force => true do |t|
     t.integer  "project_id",                :default => 0,  :null => false
