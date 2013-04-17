b5f9240defffaad4faf65da2ec6b2d84c6b520fc
Yi Zhang <yzhang.wa@gmail.com>
2013-04-17 18:47:10 +0800

    Restored the function of controlling whether to send email notification for wiki updates

--- app/models/wiki_content_observer.rb
+++ app/models/wiki_content_observer.rb
@@ -19,14 +19,14 @@ class WikiContentObserver < ActiveRecord::Observer
   def after_create(wiki_content)
     PushNotification::WikiContentNotification.notify(wiki_content, 'create')
 
-    Mailer.delay.wiki_content_added(wiki_content)
+    Mailer.delay.wiki_content_added(wiki_content) if Setting.notified_events.include?('wiki_content_added')
   end
 
   def after_update(wiki_content)
     if wiki_content.text_changed?
       PushNotification::WikiContentNotification.notify(wiki_content, 'update')
 
-      Mailer.delay.wiki_content_updated(wiki_content)
+      Mailer.delay.wiki_content_updated(wiki_content) if Setting.notified_events.include?('wiki_content_updated')
     end
   end
 end
