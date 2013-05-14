1e80ad62c9903372d1207c7419b6f2f580a82555
daveliu <dave@liageren.com>
2013-05-14 13:13:37 +0800

    fix some responsive bugs

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -398,7 +398,7 @@ module IssuesHelper
     s = ''
     unless params[:project_id] # this could be done in a better way %>
       s << '<div class="row">'
-      s << "<div class=\"span24\" style=\"font-size:150%;padding-left:30px;padding-bottom:13px;\">#{l(:issues_of_all_projects)}<br/><br/></div>"
+      s << "<div  style=\"font-size:150%;padding-left:30px;padding-bottom:13px;\">#{l(:issues_of_all_projects)}<br/><br/></div>"
       s << '</div>'
     end
     s.html_safe
