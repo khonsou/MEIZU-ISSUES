68acc0589e4d2b8304541464ea8931eadbaac17c
Ximin Yan <harryyan1238@gmail.com>
2013-07-09 14:30:51 +0800

    [#380] Fixed the bug about "orderd by overdue date" when user clicks the button "Load More" and modified taggings controller to forbid it to change issues' "updated_on" attribute.

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -4,7 +4,9 @@ class TaggingsController < ApplicationController
 
   def issue
     @issue.tag_list = params[:tags]
+    old_updated_on = Issue.find(@issue.id).updated_on
     @issue.save
+    @issue.update_attributes(:updated_on => old_updated_on)
 
     respond_to do |format|
       format.html { render :nothing => true }
