a8568c9466d53efa761fc0c2224094050d759bcc
Shaonan Zhang <ysu_zhangshaonan@outlook.com>
2013-09-04 18:19:01 +0800

    [#2326] fix bug main content flicker and responsive design

--- app/assets/javascripts/model-scroll.js
+++ app/assets/javascripts/model-scroll.js
@@ -1,24 +1,34 @@
-var isScroll = function (el) {
-             // test targets
-              var elems = el ? [el] : [document.documentElement, document.body];
-              var scrollX = false, scrollY = false;
-              for (var i = 0; i < elems.length; i++) {
-                  var o = elems[i];
-                  // test horizontal
-                  var sl = o.scrollLeft;
-                  o.scrollLeft += (sl > 0) ? -1 : 1;
-                 o.scrollLeft !== sl && (scrollX = scrollX || true);
-                 o.scrollLeft = sl;
-                 // test vertical
-                 var st = o.scrollTop;
-                 o.scrollTop += (st > 0) ? -1 : 1;
-                 o.scrollTop !== st && (scrollY = scrollY || true);
-                 o.scrollTop = st;
-             }
-             // ret
-             return {
-                 scrollX: scrollX,
-                 scrollY: scrollY
-             };
-         };
-  
+$(document).ready(function(){
+  var isScroll = function (el) {
+    // test targets
+    var elems = el ? [el] : [document.documentElement, document.body];
+    var scrollX = false, scrollY = false;
+    for (var i = 0; i < elems.length; i++) {
+      var o = elems[i];
+      // test horizontal
+      var sl = o.scrollLeft;
+      o.scrollLeft += (sl > 0) ? -1 : 1;
+      o.scrollLeft !== sl && (scrollX = scrollX || true);
+      o.scrollLeft = sl;
+      // test vertical
+      var st = o.scrollTop;
+      o.scrollTop += (st > 0) ? -1 : 1;
+      o.scrollTop !== st && (scrollY = scrollY || true);
+      o.scrollTop = st;
+    }
+    // ret
+    return {
+      scrollX: scrollX,
+      scrollY: scrollY
+    };
+  };
+
+  $('body').on('show.bs.modal', function () {
+    if(!isScroll().scrollY){
+      // no-scroll
+      $('body').addClass("modal-open-noscroll");
+    }else { 
+      $('body').removeClass("modal-open-noscroll");
+    }
+  })
+})
